bd193b23fbf4287d0cc3a87483a0f9969b03473d
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-10-15 21:50:03 +0200

    Add gunicorn

--- hipsterboard/settings.py
+++ hipsterboard/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hipsterboard.board',
+    'gunicorn',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
